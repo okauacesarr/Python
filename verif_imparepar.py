0fc526f4183fef0628e333d306802289aa4a6d21
okauacesarr <kauacesar15@gmail.com>
2025-11-25 17:33:24 -0300

    Add parity verification for user input number
    This Python program prompts the user to enter an integer and checks whether it is even or odd, displaying the corresponding message. It uses the modulo operator `%` to determine divisibility by 2 in a simple and straightforward way.

---
+++ verif_imparepar.py
@@ -0,0 +1,7 @@
+print("--- Verificador de Números Ímpar ou Par ---")
+n1 = int(input("\nDigite seu número: "))
+
+if n1 % 2 == 0:
+    print("\nSeu número é par")
+else:
+    print("\nSeu número é ímpar")
